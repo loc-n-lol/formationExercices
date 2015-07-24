aeea11a90064193180dc7b069be05802d7ea41b9
lvisse <lvisse@gmail.com>
2015-07-24 16:05:41 +0200

    StrutsFormationDatabase

---
+++ StrutsFormationDatabase/src/main/java/com/limpoto/webapps/StrutsFormationDatabase/actions/ClientAction.java
@@ -0,0 +1,35 @@
+package com.limpoto.webapps.StrutsFormationDatabase.actions;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.apache.struts2.util.ServletContextAware;
+
+import com.limpoto.webapps.StrutsFormationDatabase.beans.Client;
+import com.limpoto.webapps.StrutsFormationDatabase.utils.ClientDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ClientAction extends ActionSupport implements ServletContextAware
+{
+
+	private List<Client> clients;
+	public List<Client> getClients() {return clients;}
+	
+	private ServletContext ctx;
+	private ClientDAO clientDAO;
+
+	public String liste() 
+	{
+		clients = clientDAO.findAll();
+		return SUCCESS;
+	}
+
+	@Override
+	public void setServletContext(ServletContext arg0) 
+	{
+		ctx = arg0;
+		clientDAO = (ClientDAO)ctx.getAttribute("clientDAO");
+	}
+
+}
